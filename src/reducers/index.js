bbcc77b39bc4f99df2c9f54e589c83f2c93897c3
GIGI BERIDZE <82542634+gigi-beridze@users.noreply.github.com>
2022-04-23 15:19:22 +0400

    Add files via upload

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,8 @@
+import { combineReducers } from "redux";
+import userReducer from './userReducer';
 
+const rootReducer = combineReducers({
+    userState:userReducer,
+})
+
+export default rootReducer;
